bbbddec0c2ab4a16a0aebae054f3e01faba3f24d
kizoso <kizoso@bk.ru>
2019-02-15 16:12:35 +0300

    Calendar adding fields #28

--- components/Schedule.js
+++ components/Schedule.js
@@ -27,6 +27,8 @@ export default class Schedule extends React.Component {
     };
   }
 
+
+
   render() {
     const { value } = this.props;
 
@@ -253,48 +255,197 @@ export default class Schedule extends React.Component {
           .k-widget.k-numerictextbox.k-recur-count {\
             margin-right: 5px;\
           }\
+          .k-widget.k-dropdown, k-widget.k-datetimepicker {\
+              width: 100%;\
+              max-width: 100%;\
+          }\
           "}
         </style>
 
         <script id="customEditorTemplate" type="text/x-kendo-template">
           <div className="container-modal">
             <div className="container-fields">
-              <div className="outer">
-                <div className="k-edit-label"><label htmlFor="langArray">Язык экскурсии</label></div>
-                <div className="k-edit-field" data-container-for="langArray">
-                  <select id="langArray"
-                          data-placeholder="Type a product e.g. 'Chai'"
-                          data-value-primitive="true"
-                          data-text-field="ProductName"
-                          data-value-field="ProductID"
-                          data-bind="value: selectedProduct,
-                              source: products,
-                              visible: isVisible,
-                              enabled: isEnabled,
-                              events: {
-                                change: onChange,
-                                open: onOpen,
-                                close: onClose
-                              }"
-                          data-role="multiselect"
-                          required="required"></select>
-                  <span className="k-error"></span>
-                </div>
-              </div>
-              <hr/>
+              {/*Экскурсия*/}
               <div className="outer">
                 <div className="k-edit-label"><label htmlFor="title">Экскурсия</label></div>
                 <div data-container-for="title" className="k-edit-field">
-                  <select id="title"
-                          // data-bind="[{this.state.title}]"
+                  <input id="title"
+                         name="title"
+                         type="text"
+                         className="k-input k-textbox"/>
+                </div>
+              </div>
+
+              {/*API*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="tripApi">API</label></div>
+                <div data-container-for="tripApi" className="k-edit-field">
+                  <select id="tripApi"
+                          data-bind="value:tripApi"
                           data-role="dropdownlist"
                           data-value-field="value"
                           data-text-field="text"></select>
                 </div>
-                <div>_______________________ {this.state.title}</div>
               </div>
+
               <hr/>
+
+              {/*Весь день*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="isAllDay">Весь день</label></div>
+                <div data-container-for="isAllDay" className="k-edit-field">
+                  <input type="checkbox" name="isAllDay" data-type="boolean" data-bind="checked:isAllDay"/>
+                </div>
+              </div>
+
+              {/*Начало*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="start">Начало</label></div>
+                <div data-container-for="start" className="k-edit-field">
+                  <input type="text"
+                         data-role="datetimepicker"
+                         data-interval="15"
+                         data-type="date"
+                         data-bind="value:start,invisible:isAllDay"
+                         name="start"/>
+                  <input type="text" data-type="date" data-role="datepicker" data-bind="value:start,visible:isAllDay"
+                         name="start"/>
+                  <span data-bind="text: startTimezone"></span>
+                  <span data-for="start" className="k-invalid-msg" style={{display: 'none'}}></span>
+                </div>
+              </div>
+
+              {/*Конец*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="end">Конец</label></div>
+                <div data-container-for="end" className="k-edit-field">
+                  <input type="text" data-type="date" data-role="datetimepicker"
+                         data-bind="value:end,invisible:isAllDay"
+                         name="end" data-datecompare-msg="Окончание должно быть позже, чем начало"/>
+                  <input type="text" data-type="date" data-role="datepicker" data-bind="value:end,visible:isAllDay"
+                         name="end"
+                         data-datecompare-msg="Окончание должно быть позже, чем начало"/>
+                  <span data-bind="text: endTimezone"></span>
+                  <span data-bind="text: startTimezone, invisible: endTimezone"></span>
+                  <span data-for="end" className="k-invalid-msg" style={{display: 'none'}}></span>
+                </div>
+              </div>
+
+              {/*Прекратить продажу*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="buyTime">Прекратить продажу</label></div>
+                <div data-container-for="buyTime" className="k-edit-field">
+                  <input type="text"
+                         data-role="datetimepicker"
+                         data-interval="15"
+                         data-type="date"
+                         data-bind="value:buyTime,invisible:isAllDay"
+                         name="buyTime"/>
+                  <input type="text" data-type="date" data-role="datepicker" data-bind="value:buyTime,visible:isAllDay"
+                         name="buyTime"/>
+                  <span data-bind="text: startTimezone"></span>
+                  <span data-for="buyTime" className="k-invalid-msg" style={{display: 'none'}}></span>
+                </div>
+              </div>
+
+              {/*Расписание из API*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="isApiSchedule">Расписание из API</label></div>
+                <div data-container-for="isApiSchedule" className="k-edit-field">
+                  <div data-bind="value:isApiSchedule" name="isApiSchedule" data-role=""></div>
+                </div>
+              </div>
+
+              {/*Повторять*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="recurrenceRule">Повторять</label></div>
+                <div data-container-for="recurrenceRule" className="k-edit-field">
+                  <div data-bind="value:recurrenceRule" name="recurrenceRule" data-role="recurrenceeditor"></div>
+                </div>
+              </div>
+
+              {/*Тип времени*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="ticketType">Тип времени</label></div>
+                <div data-container-for="ticketType" className="k-edit-field">
+                  <select id="ticketType" data-bind="value:ticketType"
+                          data-role="dropdownlist" data-value-field="value" data-text-field="text"></select>
+                </div>
+              </div>
+
+              <hr/>
+
+              {/*Причал отправления*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="pierStartId">Причал отправления</label></div>
+                <div data-container-for="pierStartId" className="k-edit-field">
+                  <select id="pierStartId" data-bind="value:pierStartId"
+                          data-role="dropdownlist" data-value-field="value" data-text-field="text"></select>
+                </div>
+              </div>
+
+              {/*Причал прибытия*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="pierFinishId">Причал прибытия</label></div>
+                <div data-container-for="pierFinishId" className="k-edit-field">
+                  <select id="pierFinishId" data-bind="value:pierFinishId"
+                          data-role="dropdownlist" data-value-field="value" data-text-field="text"></select>
+                </div>
+              </div>
+
+              {/*Карта маршрута*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="mapUrl">Карта маршрута</label></div>
+                <div data-container-for="mapUrl" className="k-edit-field">
+                  <input type="text" className="k-input k-textbox" name="mapUrl" data-bind="value:mapUrl"/>
+                  {/*(может быть разная на одном направлении)*/}
+                </div>
+              </div>
+
+              {/*Транспортное средство*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="vehicleId">Транспортное средство</label></div>
+                <div data-container-for="vehicleId" className="k-edit-field">
+                  <select id="vehicleId" data-bind="value:vehicleId"
+                          data-role="dropdownlist" data-value-field="value" data-text-field="text"></select>
+                </div>
+              </div>
+
+              <hr/>
+
+              {/*Заметка для менеджера*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="description">Заметка для менеджера</label></div>
+                <div data-container-for="description" className="k-edit-field">
+                    <textarea name="description" className="k-textbox" data-bind="value:description"></textarea>
+                </div>
+              </div>
+
+              <hr/>
+
+              {/*Количество билетов*/}
+              <div className="outer">
+                <div className="k-edit-label"><label htmlFor="count">Количество билетов</label></div>
+                <div data-container-for="count" className="k-edit-field">
+                  <input name="count" className="k-textbox" data-bind="value:count"/>
+                </div>
+              </div>
             </div>
+
+            {/*Билеты*/}
+            <div className="container container-additionally">
+              <hr/>
+              <h3>Билеты</h3>
+              <div id="grid-ticket"></div>
+            </div>
+
+            {/*Доп. услуги*/}
+            <div className="container container-additionally">
+              <hr/>
+              <h3>Доп. услуги</h3>
+              <div id="grid-additional"></div>
+            </div>
+
           </div>
         </script>
 
@@ -420,7 +571,6 @@ export default class Schedule extends React.Component {
             model: {
               id: '_key',
               fields: {
-                // Языки экскурсии
                 _key:                { from: '_key',               type: 'string' },
                 _type:               { from: '_type',              type: 'string', defaultValue: 'event' },
                 start:               { from: 'start' ,             type: 'date'},
@@ -433,7 +583,6 @@ export default class Schedule extends React.Component {
                 recurrenceRule:      { from: 'recurrenceRule' },
                 recurrenceException: { from: 'recurrenceException' },
                 isAllDay:            { from: 'isAllDay',           type: 'boolean'  },
-                langArray:           { from: "langArray",          defaultValue: ['en' ,'ru'], validation: {required: true}, editable: true, nullable: true},
               }
             }
           }
@@ -448,38 +597,102 @@ export default class Schedule extends React.Component {
               title: servceObj.title.ru.name,
             },
             ()=> {
-              function onChangeLang(e) {
-                var langArr = this.value();
-                var langArrMsg = $('#langArray').closest('.k-edit-field').find('.k-error');
 
-                $('.langField').slideUp(0).prop("disabled", true);
-                $('.langField').find('.k-textbox').prop("disabled", true).prop("required", false);
-
-                if (langArr.length == 0) {
-                  langArrMsg.html("<b>Поле обязательно для заполнения!</b>");
-                } else {
-                  if (langArr.indexOf('ru') != -1) {
-                    langArrMsg.html("");
-                  } else {
-                    langArrMsg.html("<b>ВНИМАНИЕ!</b><br>Вы исключили русский язык!");
+              $('#grid-ticket').kendoGrid({
+                dataSource: new kendo.data.DataSource({
+                  pageSize: 6,
+                  data: [
+                    {
+                      productID: 1,
+                      productName: "Взрослый",
+                      price: 320,
+                      buyed: 50,
+                      description: "Bla-bla-bla…"
+                    }, {
+                      productID: 2,
+                      productName: "Детский",
+                      price: 280,
+                      buyed: 50,
+                      description: "Bla-bla-bla…"
+                    }, {
+                      productID: 3,
+                      productName: "Льготный",
+                      price: 260,
+                      buyed: 50,
+                      description: "Bla-bla-bla…"
+                    }
+                  ],
+                  autoSync: true,
+                  schema: {
+                    model: {
+                      id: "productID",
+                      fields: {
+                        productID: {editable: false, nullable: true},
+                        productName: {defaultValue: "Взрослый", validation: {required: true}},
+                        description: {defaultValue: ""},
+                        price: {type: "number", validation: {required: true, min: 0}},
+                        buyed: {editable: false, type: "number", validation: {min: 0}},
+                      }
+                    }
                   }
-
-                  langArr.forEach(function (lang) {
-                    $('.langField_' + lang).slideDown(0).prop("disabled", false);
-                    $('.langField_' + lang).find('.k-textbox').prop("disabled", false).prop("required", true);
-                  })
-                }
-              }
-
-              $("#langArray").kendoMultiSelect({
-                // dataBind: "langArray",
-                // dataSource: {transport: {read: ['ru', 'en']}},
-                value: ['ru', 'en'],
-                dataTextField: "text",
-                dataValueField: "value",
-                placeholder: "Должен быть выбран хотя бы один язык!",
-                // change: onChangeLang,
+                }),
+                pageable: false,
+                // height: 550,
+                toolbar: ["create"],
+                columns: [
+                  {field: "productName", title: "Билет"},
+                  // { field: "description", title: "Описание" },
+                  {field: "price", title: "Стоимость", format: "{0:c}"},
+                  {field: "buyed", title: "Продано"},
+                  {command: "destroy", title: " "}],
+                editable: true
               });
+
+              $('#grid-additional').kendoGrid({
+                dataSource: new kendo.data.DataSource({
+                  pageSize: 6,
+                  data: [
+                    {
+                      productID: 1,
+                      productName: "Дождевик",
+                      price: 320,
+                      description: "Bla-bla-bla…"
+                    }, {
+                      productID: 2,
+                      productName: "Зонт",
+                      price: 280,
+                      description: "Bla-bla-bla…"
+                    }, {
+                      productID: 3,
+                      productName: "Кокаин",
+                      price: 260,
+                      description: "Bla-bla-bla…"
+                    }
+                  ],
+                  autoSync: true,
+                  schema: {
+                    model: {
+                      id: "productID",
+                      fields: {
+                        productID: {editable: false, nullable: true},
+                        productName: {defaultValue: "Взрослый", validation: {required: true}},
+                        description: {defaultValue: ""},
+                        price: {type: "number", validation: {required: true, min: 0}}
+                      }
+                    }
+                  }
+                }),
+                pageable: false,
+                // height: 550,
+                toolbar: ["create"],
+                columns: [
+                  {field: "productName", title: "Билет"},
+                  // { field: "description", title: "Описание" },
+                  {field: "price", title: "Стоимость", format: "{0:c}"},
+                  {command: "destroy", title: " "}],
+                editable: true
+              });
+
             }
           );
       }
@@ -511,6 +724,8 @@ export default class Schedule extends React.Component {
   componentWillUnmount() {
     this.scheduler.destroy();
   }
+
+
 
   focus() {
     this.el.focus();
