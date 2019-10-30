75dfe852142d9f88e0e25c98e6005598d08a36fb
kizoso <kizoso@bk.ru>
2019-10-30 16:55:52 +0200

    banner

---
+++ schemas/localeBanner.js
@@ -0,0 +1,48 @@
+const supportedLanguages = [
+  {id: 'ru', title: 'Русский', isDefault: true},
+  {id: 'en', title: 'Английский'},
+  {id: 'zh', title: 'Китайский'}
+]
+
+export default {
+  name: 'localeBanner',
+  title: 'localeBanner',
+  type: 'object',
+  fieldsets: [
+    {
+      title: 'Переводы',
+      name: 'translations',
+      options: {collapsible: true}
+    }
+  ],
+  fields: supportedLanguages.map(lang => (
+    {
+      title: lang.title,
+      name: lang.id,
+      type: 'object',
+      fieldset: lang.isDefault ? null : 'translations',
+      fields: [
+        {
+          name: 'tour',
+          title: 'Экскурсия / товар',
+          type: 'reference',
+          to: { type: 'tour' },
+          required: true,
+          validation: Rule => Rule.required(),
+        },
+        {
+          name: 'titleImage',
+          title: 'Баннер-изображение',
+          type: 'image',
+          options: { hotspot: true },
+        },
+        {
+          name: 'link',
+          title: 'Ссылка',
+          type: 'string',
+          description: 'Куда ведет ссылка с баннера-изображения',
+        },
+      ]
+    }
+  ))
+}
